e5dc5596555c7b630b10255091717f929481812b
bpoongs <bpoongs@gmail.com>
2025-08-25 15:11:21 +0530

    spring boot

---
+++ spring-mvc-demo-project.zip_expanded/spring-mvc-demo-project/src/main/java/com/demo/spring_mvc_demo_project/SpringMvcDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_mvc_demo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemoProjectApplication.class, args);
+	}
+
+}
